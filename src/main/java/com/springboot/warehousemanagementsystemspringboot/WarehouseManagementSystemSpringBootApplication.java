78197dae8bece6d1b8246d5846375b45dc224c10
devkz <sbs@sbss-MacBook-Air.local>
2023-10-07 09:46:12 +0800

    spring initializer

---
+++ src/main/java/com/springboot/warehousemanagementsystemspringboot/WarehouseManagementSystemSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.warehousemanagementsystemspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseManagementSystemSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseManagementSystemSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/warehousemanagementsystemspringboot/WarehouseManagementSystemSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.warehousemanagementsystemspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseManagementSystemSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
